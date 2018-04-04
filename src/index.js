978f7d55cb7f59ab63de65037d182e2cfd714d07
szhclaye <328044021@qq.com>
2018-04-04 21:02:06 +0800

    s

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
+/*
+ * @Author: mikey.zhaopeng 
+ * @Date: 2018-04-04 20:53:44 
+ * @Last Modified by:   mikey.zhaopeng 
+ * @Last Modified time: 2018-04-04 20:53:44 
+ */
+
+
 import React from 'react'
 import { render } from 'react-dom'
